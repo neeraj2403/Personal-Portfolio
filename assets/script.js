050be47a40daf1d00dcd74d1712c4080444829e4
neeraj2403 <jayarajneeraj@gmail.com>
2020-12-03 00:43:54 +0530

    added Contact page

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,21 @@ window.addEventListener('scroll',function(){
     var header = document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY> 0);
 })
+
+// ----------------Portfolio----------------
+
+$(document).ready(function(){
+    $(".button").click(function(){
+        var value = $(this).attr("data-filter");
+        if(value == "all"){
+            $(".filter").show("100");
+        }
+        else{
+            $(".filter").not("."+value).hide("1000");
+            $(".filter").filter("."+value).show("1000");
+        }
+        $("ul.button").click(function(){
+            $(this).addClass('active').siblings().removeClass('active')
+        })
+    })
+})
